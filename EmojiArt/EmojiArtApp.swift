37d2551e914295abb68d4a7e51eb1ffa9ede1615
Xiaoyu Tai <taixiaoyu@gmail.com>
2020-07-16 15:48:33 -0700

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Sean Tai on 7/16/20.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
